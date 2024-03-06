7ecc4ee6dd8541723670059b38e8824a5876d0d7
sebastianVH <sebastian.yacono@gmail.com>
2024-03-06 19:31:36 -0300

    add cors y morgan

--- back/index.js
+++ back/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
 app.use("/",(req,res)=>{
     res.send("Server is running");
 })
 
 app.use("/test",(req,res)=>{
-    res.status(200).json({messago: "Server is running desde el deploy!"});
+    res.status(200).json({message: "Server is running desde el deploy!"});
 })
 
 app.listen(3000,()=>{
